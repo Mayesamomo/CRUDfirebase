7b0fdd4898acd6c71ea6d7542a01806a9b35c402
Michael A. Momo <48134632+Mayesamomo@users.noreply.github.com>
2021-01-31 13:31:22 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig :{
-    apiKey: "AIzaSyCF8fhmcPWHYIR_qBacwspiKkZa9n8w3qQ",
-    authDomain: "fire-crud-ca.firebaseapp.com",
-    databaseURL: "https://fire-crud-ca-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "fire-crud-ca",
-    storageBucket: "fire-crud-ca.appspot.com",
-    messagingSenderId: "398591576415",
-    appId: "1:398591576415:web:3a6511d8051078b7318eea",
-    measurementId: "G-J6B8E98CNY"
-  }
+ 
 };
 
 /*
